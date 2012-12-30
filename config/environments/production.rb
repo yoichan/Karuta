663f76958e35fc1f0dfe8df18d30b221fc15c446
Yayoi <yayoi@yayoi-virtual-machine.(none)>
2012-12-30 10:43:35 +0900

    test4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,4 @@ Karuta::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 	
-	config.logger = Logger.new(STDOUT)
-	config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 end
