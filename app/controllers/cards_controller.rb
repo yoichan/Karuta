0c357ac7e99501df86fa44b7f8b5ed8395f120a4
Yayoi <yayoi@yayoi-virtual-machine.(none)>
2013-01-07 02:14:42 +0900

    initial deploy to heroku

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -20,12 +20,16 @@ class CardsController < ApplicationController
 	
 	def question
 		@cards = Card.all
+		
 		if params[:card_id] == '000' then
+			#ランダムでカードを選択
 			@draw_card = Card.find_by_card_id('%03d' % (rand(10) + 1) )
 		else
+			#指定されたカードを選択
 			@draw_card = Card.find_by_card_id(params[:card_id])
 	
 		end
+		#インスタンス変数を使って選択されているカードをテンプレートに渡す
 		@draw_card_image = 'hyakunin-tori/tori-' << @draw_card.card_id << '.png'
 		respond_to do |format|
 			format.html
@@ -35,10 +39,15 @@ class CardsController < ApplicationController
 	end
 
 	def check
+		#リンクから上の句の歌のIDと選択された歌のIDを取得
 		draw_id = params[:draw]
 		ans_id = params[:ans]
+		
+		#インスタンス変数を使ってそれぞれのカードをテンプレートに渡す
 		@draw_card = Card.find(draw_id)
 		@card = Card.find(ans_id)
+	
+		#正誤を判定し、結果をインスタンス変数でテンプレートに渡す
 		@is_right_card
 		if draw_id == ans_id then
 			@is_right_card = true
